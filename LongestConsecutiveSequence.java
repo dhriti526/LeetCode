7aa2f351e710d46cdf712f448b8353f8b8d50fa5
Dhriti Jain <jaindhrit12@gmail.com>
2024-09-23 18:41:39 +0530

    LongestConsecutiveSequence java code

--- LongestConsecutiveSequence.java
+++ LongestConsecutiveSequence.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class LongestConsecutiveSequence {
+import java.util.HashMap;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max = 0;
+        for(int n:nums){
+            if(!map.containsKey(n)){
+                int left = map.containsKey(n-1)? map.get(n-1) : 0;
+                int right = map.containsKey(n+1)? map.get(n+1) : 0;
+                int sum = left + right + 1;
+                max = Math.max(max, sum);
+                map.put(n-left,sum);
+                map.put(n+right,sum);
+                map.put(n,sum);
+
+            }
+        }
+        return max;
+    }
 }
