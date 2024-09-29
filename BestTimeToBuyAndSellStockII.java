0bfd9123ba95a356bde4246339907b67d9bf968f
Dhriti Jain <jaindhrit12@gmail.com>
2024-09-29 11:17:51 +0530

    BestTimeToBuyAndSellStockII java code

--- BestTimeToBuyAndSellStockII.java
+++ BestTimeToBuyAndSellStockII.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class BestTimeToBuyAndSellStockII {
+public class BestTimeToBuyAndSellStockII {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int n = prices.length;
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1])
+                profit = profit + prices[i]-prices[i-1];
+        }
+        return profit;
+    }
 }
